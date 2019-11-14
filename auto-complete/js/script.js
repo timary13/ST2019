9a7d517553da9a3cb243bfba89a2bd9751f98a30
Marina Gordynets <timary99@gmail.com>
2019-11-14 15:21:03 +0300

    fix: correct all remarks

--- auto-complete/js/script.js
+++ auto-complete/js/script.js
@@ -1,88 +1,84 @@
+let Node = function () {
+    this.isWordEnd = false;
+    this.repeat = 0;
+    this.letters = new Map();
+};
+Node.prototype.setEnd = function () {
+    this.isWordEnd = true;
+};
+
+
+let Trie = function () {
+    this.root = new Node();
+};
+Trie.prototype.add = function (word, repeat, node = this.root) {
+    if (word.length == 0) {
+        node.setEnd();
+        if (repeat) {
+            node.repeat++;
+        }
+        return;
+    } else if (!node.letters.has(word[0])) {
+        node.letters.set(word[0], new Node());
+    }
+    return this.add(word.substr(1), repeat, node.letters.get(word[0]));
+};
+
+Trie.prototype.getSubTree = function (word) {
+    let node = this.root;
+    while (word.length > 1) {
+        if (!node.letters.has(word[0])) {
+            return false;
+        } else {
+            node = node.letters.get(word[0]);
+            word = word.substr(1);
+        }
+    }
+
+    if (node.letters.has(word)) {
+        return node.letters.get(word);
+    }
+};
+
+Trie.prototype.find = function (str, subNode, words = []) {
+    if (subNode.isWordEnd) {
+        words.push(str);
+        if (subNode.repeat) {
+            for (let x = 0; x < subNode.repeat; x++)
+                words.push(str);
+        }
+    }
+    if (subNode.letters.size != 0) {
+        for (let letter of subNode.letters.keys()) {
+            this.find(str.concat(letter), subNode.letters.get(letter), words);
+        }
+    }
+    return words;
+};
+
+Trie.prototype.search = function (str, node = this.root) {
+    let word = [];
+    if (node.letters.size != 0) {
+        str = str[0].toLowerCase() + str.substr(1);
+        let node = this.getSubTree(str);
+
+        if (node) {
+            word.push(...this.find(str, node));
+        }
+        str = str[0].toUpperCase() + str.substr(1);
+        node = this.getSubTree(str);
+        if (node) {
+            word.push(...this.find(str, node));
+        }
+    }
+    return word;
+};
+
 module.exports = {
+
     createAutoComplete: function (data) {
 
-        let words = [];
-
-        let Node = function () {
-            this.isWordEnd = false;
-            this.repeat = 0;
-            this.letters = new Map();
-            this.setEnd = function () {
-                this.isWordEnd = true;
-            }
-        }
-
-        let Trie = function () {
-            this.root = new Node();
-
-            this.add = function (word, repeat, node = this.root) {
-                if (word.length == 0) {
-                    node.setEnd();
-                    if (repeat) {
-                        node.repeat++;
-                    }
-                    return;
-                } else if (!node.letters.has(word[0])) {
-                    node.letters.set(word[0], new Node());
-                    return this.add(word.substr(1), repeat, node.letters.get(word[0]));
-                } else {
-                    return this.add(word.substr(1), repeat, node.letters.get(word[0]));
-                }
-            };
-
-            this.getSubTree = function (word) {
-                let node = this.root;
-                while (word.length > 1) {
-                    if (!node.letters.has(word[0])) {
-                        return false;
-                    } else {
-                        node = node.letters.get(word[0]);
-                        word = word.substr(1);
-                    }
-                }
-
-                if (node.letters.has(word))
-                    return node.letters.get(word);
-                else return null;
-            }
-
-            this.search = function (str, node = this.root) {
-                if (node.letters.size != 0) {
-                    let find = function (str, subNode) {
-                        if (subNode.isWordEnd) {
-                            words.push(str);
-                            if (subNode.repeat) {
-                                for (let x = 0; x < subNode.repeat; x++)
-                                    words.push(str);
-                            }
-                        }
-                        if (subNode.letters.size != 0) {
-                            for (let letter of subNode.letters.keys()) {
-                                find(str.concat(letter), subNode.letters.get(letter));
-                            }
-                        } else {
-                            return;
-                        }
-                    }
-
-                    str = str[0].toLowerCase() + str.substr(1);
-                    let node = this.getSubTree(str);
-
-                    if (node) {
-                        find(str, node);
-                    }
-                    str = str[0].toUpperCase() + str.substr(1);
-                    node = this.getSubTree(str);
-                    if (node) {
-                        find(str, node);
-                    }
-                }
-            }
-
-        }
-
-        let tree;
-        tree = new Trie();
+        const tree = new Trie();
         data.forEach(item => {
             //repeat
             if (tree.getSubTree(item)) {
@@ -93,14 +89,12 @@ module.exports = {
         });
 
         return function (str) {
-            words = [];
-            if (str != undefined) {
-                if (str.trim() != '') {
-                    tree.search(str);
-                }
+            let result = [];
+            if (str != undefined && str.trim() != '') {
+                result.push(...tree.search(str));
             }
-            return words;
+            return result;
         }
     }
-}
+};
 
