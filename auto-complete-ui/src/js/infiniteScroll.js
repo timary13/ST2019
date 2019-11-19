65fe154f8a69a5d3fbfa9b431205b76ae6852a5b
Marina Gordynets <timary99@gmail.com>
2019-11-19 19:08:52 +0300

    feat: inplement in es6 style

--- auto-complete-ui/src/js/infiniteScroll.js
+++ auto-complete-ui/src/js/infiniteScroll.js
@@ -1,62 +1,64 @@
 import Iterator from './iterator';
 
-let InfiniteScroll = function (word) {
-    this.word = word;
-    this.items = document.getElementById('items');
-    this.iterator = new Iterator(this.word);
-};
+class InfiniteScroll {
+    constructor(word) {
+        this.word = word;
+        this.items = document.getElementById('items');
+        this.iterator = new Iterator(this.word);
+    }
 
-InfiniteScroll.prototype.createStartItems = function () {
-    for(let i = 0; i < 5; i++) {
-        if(this.iterator.hasNext()) {
-            let item = this.createItem(this.iterator.next(), this.word.length);
+    createStartItems() {
+        for (let i = 0; i < 5; i++) {
+            if (this.iterator.hasNext()) {
+                let item = this.createItem(this.iterator.next(), this.word.length);
+                this.items.appendChild(item);
+            }
+        }
+    };
+
+    createDiv(className) {
+        let div = document.createElement('div');
+        div.className = className;
+        return div;
+    };
+
+    static closeAllLists(item) {
+        let items = document.getElementsByClassName("autocomplete-items");
+        for (let i = 0; i < items.length; i++) {
+            if (item != items[i]) {
+                items[i].parentNode.removeChild(items[i]);
+            }
+        }
+    };
+
+    createItem(next = this.iterator.next(), length = this.word.length) {
+        let item = this.createDiv("item");
+        item.innerHTML = "<strong>" + next.substr(0, length) + "</strong>";
+        item.innerHTML += next.substr(length);
+        item.innerHTML += "<input type='hidden' value='" + next + "'>";
+        item.addEventListener("click", function (e) {
+            document.getElementsByTagName('input')[0].value = next;
+            InfiniteScroll.closeAllLists();
+        });
+        return item;
+    };
+
+    appendFirst() {
+        if (this.iterator.hasNext()) {
+            let item = this.createItem();
+            this.items.firstChild.remove();
             this.items.appendChild(item);
         }
-    }
-};
+    };
 
-InfiniteScroll.prototype.createDiv = function(className) {
-    let div = document.createElement('div');
-    div.className = className;
-    return div;
-};
-
-InfiniteScroll.closeAllLists = function(item) {
-    let items = document.getElementsByClassName("autocomplete-items");
-    for (let i = 0; i < items.length; i++) {
-        if (item != items[i]) {
-            items[i].parentNode.removeChild(items[i]);
+    appendLast() {
+        if (this.iterator.hasPreviousFive()) {
+            let item = this.createItem(this.iterator.previousFiveItem());
+            this.items.lastChild.remove();
+            this.items.insertBefore(item, this.items.firstChild);
         }
-    }
-};
+    };
 
-InfiniteScroll.prototype.createItem = function (next = this.iterator.next(), length = this.word.length) {
-    let item = this.createDiv("item");
-    item.innerHTML = "<strong>" + next.substr(0, length) + "</strong>";
-    item.innerHTML += next.substr(length);
-    item.innerHTML += "<input type='hidden' value='" + next + "'>";
-    item.addEventListener("click", function (e) {
-        document.getElementsByTagName('input')[0].value = next;
-        InfiniteScroll.closeAllLists();
-    });
-    return item;
-};
-
-InfiniteScroll.prototype.appendFirst = function () {
-    if (this.iterator.hasNext()) {
-        let item = this.createItem();
-        this.items.firstChild.remove();
-        this.items.appendChild(item);
-    }
-};
-
-InfiniteScroll.prototype.appendLast = function () {
-    if (this.iterator.hasPreviousFive()) {
-        let item = this.createItem(this.iterator.previousFiveItem());
-        this.items.lastChild.remove();
-        this.items.insertBefore(item, this.items.firstChild);
-    }
-};
-
+}
 
 export default InfiniteScroll;
--- auto-complete-ui/src/js/iterator.js
+++ auto-complete-ui/src/js/iterator.js
@@ -1,35 +1,37 @@
-import { createAutoComplete } from '../../../auto-complete/js/script';
+import {createAutoComplete} from '../../../auto-complete/js/script';
 import cities from '../cities';
 
 const autocomplete = createAutoComplete(cities);
 
-let Iterator = function (word) {
-    this.counter = -1;
-    this.result = autocomplete(word);
-};
+class Iterator {
+    constructor(word) {
+        this.counter = -1;
+        this.result = autocomplete(word);
+    };
 
-Iterator.prototype.hasNext = function () {
-    return ((this.counter + 1) <= (this.result.length - 1));
-};
+    hasNext() {
+        return ((this.counter + 1) <= (this.result.length - 1));
+    };
 
-Iterator.prototype.next = function () {
-    this.counter++;
-    return this.result[this.counter];
-};
+    next() {
+        this.counter++;
+        return this.result[this.counter];
+    };
 
-Iterator.prototype.hasPreviousFive = function () {
-    return ((this.counter - 5) >= 0);
-};
+    hasPreviousFive() {
+        return ((this.counter - 5) >= 0);
+    };
 
-Iterator.prototype.previousFiveItem = function () {
-    let count = this.counter - 5;
-    this.counter--;
-    return this.result[count];
-};
+    previousFiveItem() {
+        let count = this.counter - 5;
+        this.counter--;
+        return this.result[count];
+    };
 
-Iterator.prototype.isNotEmpty = function () {
-    return ((this.result.length === 0) ? false : true);
-};
+    isNotEmpty() {
+        return ((this.result.length === 0) ? false : true);
+    };
 
+}
 
 export default Iterator;
